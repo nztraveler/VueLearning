7e08eacd3000ae1c778564dadf92afb2115e9205
husihao <husihao1994@gmail.com>
2017-12-26 14:46:18 +0800

    新增：Vue框架文件

--- js/index.js
+++ js/index.js
@@ -8,7 +8,34 @@ var vm = new Vue({
       return this.message.split('').reverse().join('')
     }
   }
-  
+})
+
+var vm2 = new Vue({
+  el: '#example-3',
+  data: {
+    checkedNames: {123: 123}
+  },
+})
+
+let vm3 = new Vue({
+  el: '#example-4',
+  data: {
+    picked: {123: 123}
+  }
+})
+
+new Vue({
+  el: '#example-6',
+  data: {
+    selected: []
+  }
+})
+
+new Vue({
+  el: '#example-7',
+  data: {
+    message: ''
+  }
 })
 
 // console.log(vm.reversedMessage) // => 'olleH'
