01594afd6feda0fab6ee75689688fbfeac7fd64d
brian <brian@cflab.net>
2018-01-16 22:41:41 +0800

    1

---
+++ js/vuex.js
@@ -0,0 +1,44 @@
+const store = new Vuex.store({
+  state: {
+    count:0
+  },
+  mutations:{
+    increment(state){
+      state.count++
+    }
+  }
+})
+store.commit('increment')
+console.log(store.state.count)
+
+modules: {
+  foo: {
+    namespaced: true,
+
+      getters: {
+      // 在这个模块的 getter 中，`getters` 被局部化了
+      // 你可以使用 getter 的第四个参数来调用 `rootGetters`
+      someGetter (state, getters, rootState, rootGetters) {
+        getters.someOtherGetter // -> 'foo/someOtherGetter'
+        rootGetters.someOtherGetter // -> 'someOtherGetter'
+      },
+      someOtherGetter: state => { ... }
+    },
+
+    actions: {
+      // 在这个模块中， dispatch 和 commit 也被局部化了
+      // 他们可以接受 `root` 属性以访问根 dispatch 或 commit
+      someAction ({ dispatch, commit, getters, rootGetters }) {
+        getters.someGetter // -> 'foo/someGetter'
+        rootGetters.someGetter // -> 'someGetter'
+
+        dispatch('someOtherAction') // -> 'foo/someOtherAction'
+        dispatch('someOtherAction', null, { root: true }) // -> 'someOtherAction'
+
+        commit('someMutation') // -> 'foo/someMutation'
+        commit('someMutation', null, { root: true }) // -> 'someMutation'
+      },
+      someOtherAction (ctx, payload) { ... }
+    }
+  }
+}
